e1b27269745a0e2f6e645b17a7ce4ce35f7b4d5e
Mansi <mansibhiwapurkar97@gmail.com>
2024-05-09 17:48:48 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart'; // Make sure the path is correct based on your project structure
+import 'SignUpPage.dart'; // Make sure this path is correct
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Family Social App',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.green,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.standard,
+      ),
+      initialRoute: '/', // default is login page
+      routes: {
+        '/': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(), // Adding the signup route
+      },
+    );
+  }
+}
